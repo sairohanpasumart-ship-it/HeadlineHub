14a8bec7ad396f9cf541565046d910b4bfbd8a9c
sairohanpasumart-ship-it <sairohan.pasumart@gmail.com>
2026-07-30 11:33:28 -0500

    Added More News Feeds

--- main.py
+++ main.py
@@ -2,29 +2,49 @@ import feedparser
 import sqlite3
 
 def main():
-    feed_url = "https://feeds.texastribune.org/feeds/main/"
-    news_feed = feedparser.parse(feed_url)
     connection = sqlite3.connect("headlinelab.db")
     cursor = connection.cursor()
     added = 0
-    for article in news_feed.entries:
-        cursor.execute("""
-            INSERT OR IGNORE INTO articles
-            (title, link, source, published, summary)
-            VALUES (?, ?, ?, ?, ?)
-        """, (
-            article.title,
-            article.link,
-            "Texas Tribune",
-            article.get("published", ""),
-            article.get("summary", "")
-        ))
-        if cursor.rowcount == 1:
-            added += 1
+    feeds = [
+         (
+        "Texas Tribune",
+        "https://feeds.texastribune.org/feeds/main/"
+    ),
+    (
+        "BBC World",
+        "https://feeds.bbci.co.uk/news/world/rss.xml"
+    ),
+    (
+        "BBC Business",
+        "https://feeds.bbci.co.uk/news/business/rss.xml"
+    ),
+    (
+        "BBC Technology",
+        "https://feeds.bbci.co.uk/news/technology/rss.xml"
+    )
+    ]
+    for source, feed_url in feeds:
+        news_feed = feedparser.parse(feed_url)
+    
+    
+        for article in news_feed.entries:
+            cursor.execute("""
+                INSERT OR IGNORE INTO articles
+                (title, link, source, published, summary)
+                VALUES (?, ?, ?, ?, ?)
+            """, (
+                article.title,
+                article.link,
+                source,
+                article.get("published", ""),
+                article.get("summary", "")
+            ))
+            if cursor.rowcount == 1:
+                added += 1
 
-    connection.commit()
+        connection.commit()
+       
     connection.close()
-
     print(f"Added {added} new articles")
 
 
