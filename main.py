7107917bf41ee1420f72e0e22b5a4a384b1c912a
sairohanpasumart-ship-it <sairohan.pasumart@gmail.com>
2026-07-31 13:53:31 -0500

    Bug fixes

--- main.py
+++ main.py
@@ -14,40 +14,32 @@ supabase = create_client(url, key)
 def main():
     added = 0
     feeds = [
-         (
-        "Texas Tribune",
-        "https://feeds.texastribune.org/feeds/main/"
-    ),
-    (
-        "BBC World",
-        "https://feeds.bbci.co.uk/news/world/rss.xml"
-    ),
-    (
-        "BBC Business",
-        "https://feeds.bbci.co.uk/news/business/rss.xml"
-    ),
-    (
-        "BBC Technology",
-        "https://feeds.bbci.co.uk/news/technology/rss.xml"
-    )
+        (
+            "Texas Tribune",
+            "https://feeds.texastribune.org/feeds/main/"
+        ),
+        (
+            "BBC World",
+            "https://feeds.bbci.co.uk/news/world/rss.xml"
+        ),
+        (
+            "BBC Business",
+            "https://feeds.bbci.co.uk/news/business/rss.xml"
+        ),
+        (
+            "BBC Technology",
+            "https://feeds.bbci.co.uk/news/technology/rss.xml"
+        )
     ]
     for source, feed_url in feeds:
         news_feed = feedparser.parse(feed_url)
 
         for article in news_feed.entries:
-            link = article.get("link", "").lower()
-            title = article.get("title", "").lower()
-            summary = article.get("summary", "").lower()
+            link = article.get("link", "")
+            title = article.get("title", "")
+            summary = article.get("summary", "")
 
-            if (
-                "/videos/" in link
-                or "/sounds/" in link
-                or "/programmes/" in link
-                or "/iplayer/" in link
-                or "podcast" in title
-                or "podcast" in summary
-                or "bbc sounds" in summary
-            ):
+            if ("/videos/" in link or "/sounds/" in link or "/programmes/" in link or "/iplayer/" in link or "podcast" in title or "podcast" in summary or "bbc sounds" in summary):
                 continue
             article_data = {
                 "title": article.title,
